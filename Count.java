697b358864e3e2aa479e079ab39e109195910a48
Cordell <cordellbonnieux@gmail.com>
2021-06-17 09:23:48 -0700

    Created first file for assignment 3

---
+++ Count.java
@@ -0,0 +1,29 @@
+**/
+*
+*
+*
+*
+*/
+import java.util.Scanner;
+public class Count {
+    public static void main(String[] args) {
+
+        //
+        Scanner input = new Scanner(System.in);
+
+        //
+        System.out.println("Enter a string: ");
+
+        //
+        String userString = input.nextLine();
+
+        // variables to count the number of each character type
+        int digitCounter = 0, uppercaseCounter = 0;
+
+        // check each character in the string
+        for (int i = 0; i <= userString.length(); i++) {
+
+        }
+
+    }
+}
