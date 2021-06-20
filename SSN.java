9296751580fa7c81bbf6a771ffd14d27d15ec0be
Cordell <cordellbonnieux@gmail.com>
2021-06-20 12:17:32 -0700

    Got the logic working in SSN, placed all the checks in a new method called checkSSN.

--- SSN.java
+++ SSN.java
@@ -19,28 +19,50 @@ public class SSN {
         // read SSN
         String socialSecurity = input.nextLine();
 
+        // check if SSN is valid
+        Boolean isValid = checkSSN(socialSecurity);
 
-        // check if first digit is 0
-        if (((int)socialSecurity.charAt(0)) == 0) {
-
-            System.out.println("invalid format - 1st check");
-            main(args);
-
-        //  checks if 4th digit is less than 1
-        } else if (((int)socialSecurity.charAt(4)) < 1) {
-
-            System.out.println("invalid format - 2nd check");
-            main(args);
-
-        // check if dashes are formated is correctly
-        } else if (socialSecurity.charAt(3) != "-" || socialSecurity.charAt(7) != "-") {
-
-            System.out.println("invalid format - 2nd check");
-            main(args);
-
-        // check if 
-        }
-
+        // print to console if true
+        if (isValid)
+            System.out.println(socialSecurity + " is valid.");
         
+    }
+
+    public static Boolean checkSSN(String socialSecurity) {
+
+                // check if first digit is 0
+                if (((int)socialSecurity.charAt(0)) == 0) {
+
+                    System.out.println("invalid format - 1st check");
+                    main(null);
+        
+                //  checks if 4th digit is less than 1
+                } else if (((int)socialSecurity.charAt(4)) < 1) {
+        
+                    System.out.println("invalid format - 2nd check");
+                    main(null);
+        
+                // check if dashes are formated is correctly
+                } else if (((char)socialSecurity.charAt(3)) != '-' || ((char)socialSecurity.charAt(7)) != '-') {
+        
+                    System.out.println("invalid format - 2nd check");
+                    main(null);
+        
+                // check if the rest of the characters are digits
+                } else {
+        
+                    for (int i = 0; i < 10; i++) {
+        
+                        if (((int)socialSecurity.charAt(i)) != 3 || ((int)socialSecurity.charAt(i)) != 7) {
+        
+                            System.out.println("social security at index " + i + " equals " + ((int)socialSecurity.charAt(i)));
+        
+                        }
+        
+                    }
+        
+                }
+
+                return true;
     }
 }
