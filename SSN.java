707cea00992b77a5eb2b0674f89354234b3691dc
Cordell <cordellbonnieux@gmail.com>
2021-06-20 11:59:07 -0700

    Started putting together the second part of the assignmet (SSN).

--- SSN.java
+++ SSN.java
@@ -10,5 +10,37 @@ import java.util.Scanner;
 public class SSN {
     public static void main(String[] args) {
 
+        // create scanner object
+        Scanner input = new Scanner(System.in);
+
+        // prompt user
+        System.out.print("Please enter a valid SSN (format: xxx-xxx-xxx): ");
+
+        // read SSN
+        String socialSecurity = input.nextLine();
+
+
+        // check if first digit is 0
+        if (((int)socialSecurity.charAt(0)) == 0) {
+
+            System.out.println("invalid format - 1st check");
+            main(args);
+
+        //  checks if 4th digit is less than 1
+        } else if (((int)socialSecurity.charAt(4)) < 1) {
+
+            System.out.println("invalid format - 2nd check");
+            main(args);
+
+        // check if dashes are formated is correctly
+        } else if (socialSecurity.charAt(3) != "-" || socialSecurity.charAt(7) != "-") {
+
+            System.out.println("invalid format - 2nd check");
+            main(args);
+
+        // check if 
+        }
+
+        
     }
 }
