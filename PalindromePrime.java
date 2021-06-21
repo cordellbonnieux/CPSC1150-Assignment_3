4db227cd73a8836cc140c4ace7c9b9bf27b254e9
Cordell <cordellbonnieux@gmail.com>
2021-06-20 17:36:20 -0700

    PalindromePrimes is mostly working, just need to iron out the kinks

--- PalindromePrime.java
+++ PalindromePrime.java
@@ -9,18 +9,37 @@ import java.util.ArrayList;
 public class PalindromePrime {
     public static void main(String[] args) {
 
-        // create array of primes
+        // create array to store prime numbers
         ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
 
+        // populate primeNumbers with all prime numbers between 0 and 99999
         for (int i = 0; i < 99999; i++) {
+
             Boolean prime = isPrime(i);
             if (prime) primeNumbers.add(i);
 
         }
 
+        // create array to store palindrome primes
+        ArrayList<Integer> palindromePrimes = new ArrayList<Integer>();
+
+        // find all the palindromes in primeNumbers and add them to palindomePrimes
+        for (int y = 0; y < primeNumbers.size(); y++) {
+
+            Boolean palindrome = isPalindrome(primeNumbers.get(y));
+            if (palindrome) palindromePrimes.add(primeNumbers.get(y));
+
+        }
+
+        // for now, print all the palindrome primes
+        for (int w = 0; w < palindromePrimes.size(); w++) {
+            System.out.print(", " + palindromePrimes.get(w));
+        }
+
 
     }
     public static Boolean isPrime(int i) {
+
 
         if (i == 0 || i == 1) {
 
@@ -29,7 +48,7 @@ public class PalindromePrime {
         } else {
 
             for (int x = 2; x <= (i/2); x++) {
-                
+
                 if (i % x == 0) {
 
                     return false;
@@ -41,8 +60,24 @@ public class PalindromePrime {
                 }
             }
         }
+
+        return false;
     }
     public static Boolean isPalindrome(int num) {
 
+        String number = String.valueOf(num);
+
+        int length = number.length();
+
+        int half = length / 2;
+
+        for (int z = 0; z < half; z++) {
+
+            if (!(number.charAt(z) == number.charAt((length - 1) - z)))
+                return false;
+
+        }
+
+        return true;
     }
 }
